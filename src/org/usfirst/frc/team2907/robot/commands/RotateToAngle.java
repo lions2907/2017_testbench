4acec9571d20327c4f7fc2bfed06c8edc83ce624
William <williamzulueta@rocketmail.com>
2017-01-12 21:19:01 -0800

    added files

---
+++ src/org/usfirst/frc/team2907/robot/commands/RotateToAngle.java
@@ -0,0 +1,74 @@
+package org.usfirst.frc.team2907.robot.commands;
+
+import org.usfirst.frc.team2907.robot.Robot;
+
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class RotateToAngle extends Command {
+
+	static final double kP = 0.03;
+	static final double kI = 0.00;
+	static final double kD = 0.00;
+	static final double kF = 0.00;
+	static final double kToleranceDegrees = 2.0f;
+	
+	private PIDController pidController;
+	private PIDOutput output;
+	
+	private double destDegrees;
+
+	public RotateToAngle(double degrees) {
+		super("RotateToAngle");
+		requires(Robot.driveTrain);
+		
+		output = new PIDOutput();
+		pidController = new PIDController(kP, kI, kD, Robot.driveTrain.sensorBoard, output);
+		pidController.setInputRange(-180, 180);
+		pidController.setOutputRange(-1, 1);
+		pidController.setAbsoluteTolerance(kToleranceDegrees);
+		pidController.setContinuous(true);
+		
+		destDegrees = degrees;
+		// Use requires() here to declare subsystem dependencies
+		// eg. requires(chassis);
+	}
+
+	// Called just before this Command runs the first time
+	protected void initialize() {
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		pidController.setSetpoint(destDegrees);
+		pidController.enable();
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return false;
+	}
+
+	// Called once after isFinished returns true
+	protected void end() {
+		pidController.disable();
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+		end();
+	}
+	
+	class PIDOutput implements edu.wpi.first.wpilibj.PIDOutput
+	{
+
+		public void pidWrite(double output) {
+			Robot.driveTrain.mechDrive(Robot.oi.leftStick.getX(), Robot.oi.leftStick.getY(), output, Robot.driveTrain.sensorBoard.getAngle());
+		}
+		
+	}
+}
---
+++ src/org/usfirst/frc/team2907/robot/subsystems/DriveTrain.java
@@ -0,0 +1,55 @@
+package org.usfirst.frc.team2907.robot.subsystems;
+
+import org.usfirst.frc.team2907.robot.Robot;
+import org.usfirst.frc.team2907.robot.RobotMap;
+import org.usfirst.frc.team2907.robot.commands.MechDrive;
+
+import com.ctre.CANTalon;
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.SerialPort.Port;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class DriveTrain extends Subsystem 
+{
+	private CANTalon left1 = new CANTalon(RobotMap.talon_left_1);
+	private CANTalon left2 = new CANTalon(RobotMap.talon_left_2);
+	private CANTalon right1 = new CANTalon(RobotMap.talon_right_1);
+	private CANTalon right2 = new CANTalon(RobotMap.talon_right_2);
+	
+	public RobotDrive robotDrive;
+	
+	public AHRS sensorBoard;
+    // Put methods for controlling this subsystem
+	
+    // here. Call these from Commands.
+	
+	public DriveTrain()
+	{
+		robotDrive = new RobotDrive(left1, left2, right1, right2);
+		try
+		{
+			sensorBoard = new AHRS(Port.kUSB1);
+		} catch (Exception e)
+		{
+			System.out.println("Error instantating sensorBoard : " + e.getMessage());
+		}
+	}
+
+    public void initDefaultCommand() {
+    	setDefaultCommand(new MechDrive());
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    public void mechDrive(double x, double y, double twist, double gyroAngle)
+    {
+    	robotDrive.mecanumDrive_Cartesian(x, y, twist, gyroAngle);
+    }
+}
+
